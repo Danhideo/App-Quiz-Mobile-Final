a97a0e077ed21c4c45bf6078726a0e9506a511d2
Danhideo <danielhideotoma@gmail.com>
2023-12-06 18:37:27 -0300

    ok

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Qual é o maior oceano do mundo?",
+            "Em qual continente está localizado o Brasil?",
+            "Qual é a capital de Bangladesh?",
+            "Qual é o maior país da Europa em área territorial?"
+    };
+
+    public static String choices[][] = {
+            {"O Pacífico","O Atlântico","O Índico.","O Ártico"},
+            {"América do Norte","América do Sul","Europa","África"},
+            {"Beirute","Damasco","Bogotá","Daca"},
+            {"Alemanha","França","Itália","Rússia"}
+    };
+
+    public static String correctAnswers[] = {
+            "O Pacífico",
+            "América do Sul",
+            "Daca",
+            "Rússia"
+    };
+
+}
